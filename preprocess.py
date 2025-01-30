0a877ec551cabbfa00983b13a1ce6ce4c65489bc
Kaviya2205 <153165070+Kaviya2205@users.noreply.github.com>
2025-01-30 09:14:02 +0530

    Create preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,15 @@
+import re
+import string
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download("stopwords")
+stop_words = set(stopwords.words("english"))
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"http\S+|www\S+|https\S+", "", text, flags=re.MULTILINE)
+    text = re.sub(r"@\w+|\#", "", text)
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    text = " ".join([word for word in text.split() if word not in stop_words])
+    return text
